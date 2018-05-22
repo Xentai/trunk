25dcb5f1bfeff55b15badb74bb5f4c8fac53dd48
Xentai <stenclient@gmail.com>
2018-05-22 14:23:27 +0300

    db fix

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -791,7 +791,7 @@ $settings['entity_update_batch_size'] = 50;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'helen',
+  'database' => 'trunk',
   'username' => 'root',
   'password' => '',
   'prefix' => '',
